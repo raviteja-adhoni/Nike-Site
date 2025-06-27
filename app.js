7c933b22fab8763bc3d924cc13c0e48ea10db967
raviteja-adhoni <raviteja.adhoni88@gmail.com>
2025-06-28 00:51:39 +0530

    Initial commit

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res, next) => {
   res.render("index");
 });
+app.use((req, res, next) => {
+  res.status(404).send("Page Not Found 404");
+});
 
 app.listen(3000, () => {
   console.log("Server started...!");
